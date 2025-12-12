def602345d52980974b23a5b1269fb6f297d7a7d
Vladislav <103582795+harfury@users.noreply.github.com>
2025-12-12 16:29:19 +0300

    Update cron.sh

--- scripts/scripts/services/cron.sh
+++ scripts/scripts/services/cron.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# netmon/scripts/services/cron.sh
 
+crond -f &
+
+# logrotate (каждый день)
+while true; do
+  sleep 86400  # 24h
+  logrotate -f <<EOF
+/opt/netmon/log/*.log {
+  daily
+  rotate 7
+  compress
+  missingok
+  notifempty
+}
+EOF
+done
